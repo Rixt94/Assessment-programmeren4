aea76ed0a29fb4a2a567a20ce53a8502dc0371b6
Rixt94 <rixt@denhoedt.nl>
2018-05-14 22:24:58 +0200

    The studentenhuis endpoints

--- controllers/deelnemers_controller.js
+++ controllers/deelnemers_controller.js
@@ -3,8 +3,24 @@ const assert = require('assert');
 var db = require('../config/db');
 
 module.exports = {
-  postParticipant(req, res, next) {
 
+  postParticipant(req, res, next) {
+    console.log('added particitant to the mail');
+
+    const UserID = req.body.UserID;
+    const StudentenhuisID = req.body.StudentenhuisID;
+    const MaaltijdID = req.body.MaaltijdID;
+    console.log( voornaam +' ' + achternaam + 'is toegevoed aan de lijst');
+
+    db.query('INSERT INTO `deelnemers` (UserID, StudentenhuisID, MaaltijdID) VALUES (' + UserID + ', ' + StudentenhuisID +', ' + MaaltijdID + ')', (error, rows, fields) => {
+        if (error) {
+            next(error);
+        } else {
+            res.status(200).json({
+                result: rows
+            }).end();
+        }
+    });
   },
   getAllParticipants(req, res, next) {
     console.log("get all participants");
@@ -20,6 +36,16 @@ module.exports = {
     });
   },
   deleteParticipant(req, res, next) {
+    const UserID = req.body.UserID;
+    const StudentenhuisID = req.body.StudentenhuisID;
+    const MaaltijdID = req.body.MaaltijdID;
 
-  }
+        db.query('DELETE FROM `deelnemers` WHERE UserID = ' + UserID + ' AND StudentenhuisID = ' + StudentenhuisID + ' AND MaaltijdID =' + MaaltijdID, (error, rows, fields) => {
+            if (error) {
+                next(error);
+            } else {
+                console.log('You have deleted nr: ' + id);
+                res.status(200).end();
+            };
+        });
 }
