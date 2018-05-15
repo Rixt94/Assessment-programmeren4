224187dad26e3c15a44005d91dd4d962372affec
Rixt94 <rixt@denhoedt.nl>
2018-05-15 15:30:38 +0200

    Verbeterde endpoints voor het deelnemers, hij doet het echter nog niet optimaal

--- controllers/deelnemers_controller.js
+++ controllers/deelnemers_controller.js
@@ -1,6 +1,9 @@
-const auth =  require('../auth/authentication');
+//const auth =  require('../auth/authentication');
 const assert = require('assert');
-var db = require('../config/db');
+const db = require('../config/db');
+const Participant = require('../model/Participant')
+
+let participantslist = [];
 
 module.exports = {
 
@@ -11,6 +14,20 @@ module.exports = {
     const StudentenhuisID = req.body.StudentenhuisID;
     const MaaltijdID = req.body.MaaltijdID;
     console.log( voornaam +' ' + achternaam + 'is toegevoed aan de lijst');
+
+      try{
+          expect(UserID).to.be.at.least(0 ["Id munst be a number and above 0"]);
+          expect(StudentenhuisID).to.be.at.least(0 ["UserId munst be a number and above 0"]);
+          expect(MaaltijdID).to.be.at.least(0 ["UserId munst be a number and above 0"]);
+          expect(UserID).to.not.to.be.empty;
+          expect(StudentenhuisID).to.not.to.be.empty;
+          expect(MaaltijdID).to.not.to.be.empty;
+          expect(res.body).should.be.a('object');
+      }catch(ex) {
+          const error = new ApiError(ex.toString(), 422)
+          next(error)
+          return
+      }
 
     db.query('INSERT INTO `deelnemers` (UserID, StudentenhuisID, MaaltijdID) VALUES (' + UserID + ', ' + StudentenhuisID +', ' + MaaltijdID + ')', (error, rows, fields) => {
         if (error) {
@@ -24,7 +41,7 @@ module.exports = {
   },
   getAllParticipants(req, res, next) {
     console.log("get all participants");
-    
+
     db.query('SELECT * FROM user', (error, rows, fields) => {
       if(error) {
         next(error);
@@ -39,6 +56,20 @@ module.exports = {
     const UserID = req.body.UserID;
     const StudentenhuisID = req.body.StudentenhuisID;
     const MaaltijdID = req.body.MaaltijdID;
+
+      try{
+          expect(UserID).to.be.at.least(0 ["Id munst be a number and above 0"]);
+          expect(StudentenhuisID).to.be.at.least(0 ["UserId munst be a number and above 0"]);
+          expect(MaaltijdID).to.be.at.least(0 ["UserId munst be a number and above 0"]);
+          expect(UserID).to.not.to.be.empty;
+          expect(StudentenhuisID).to.not.to.be.empty;
+          expect(MaaltijdID).to.not.to.be.empty;
+          expect(res.body).should.be.a('object');
+      }catch(ex) {
+          const error = new ApiError(ex.toString(), 422)
+          next(error)
+          return
+      }
 
         db.query('DELETE FROM `deelnemers` WHERE UserID = ' + UserID + ' AND StudentenhuisID = ' + StudentenhuisID + ' AND MaaltijdID =' + MaaltijdID, (error, rows, fields) => {
             if (error) {
