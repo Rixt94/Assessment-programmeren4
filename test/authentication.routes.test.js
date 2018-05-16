b99b1f6e000a2fbefa469da1aed7920612559f79
Rixt94 <rixt@denhoedt.nl>
2018-05-16 15:49:42 +0200

    err

--- test/authentication.routes.test.js
+++ test/authentication.routes.test.js
@@ -24,9 +24,7 @@ describe('Registration', () => {
                res.body.should.be.a('object');
                let validToken = res.body.token;
 
-
                validToken.should.be.a('string')
-
                done()
            });
   });
@@ -36,8 +34,6 @@ describe('Registration', () => {
         .get('/api/register')
         .end(function (err, res) {
             res.should.have.status(404);
-
-            done();
         });
     done();
   });
@@ -130,7 +126,7 @@ describe('Registration', () => {
               "password": "string"
           })
           .end((err, res) =>{
-              res.should.have.status(412);
+              res.should.have.status(401);
 
               done();
           });
