c63af91cc18ade4fd6b510b46720bfa64b0aee60
Rixt94 <rixt@denhoedt.nl>
2018-05-16 07:23:04 +0200

    Login testcases

--- test/authentication.routes.test.js
+++ test/authentication.routes.test.js
@@ -11,43 +11,32 @@ let validToken;
 
 describe('Registration', () => {
    it('should return a token when providing valid information', (done) => {
-  //     chai.request(server)
-  //         .post('/api/register')
-  //         .send(register_details)
-  //         .end((err, res) => { // when we get a response from the endpoint
-  //             // in other words,
-  //             // the res object should have a status of 201
-  //             res.should.have.status(201);
-  //             // the property, res.body.state, we expect it to be true.
-  //             expect(res.body.state).to.be.true;
-  //
-  //             // follow up with login
-  //             chai.request(server)
-  //                 .post('/api/v1/auth/login')
-  //                 .send(login_details)
-  //                 .end((err, res) => {
-  //                     res.should.have.status(200);
-  //                     expect(res.body.state).to.be.true;
-  //                     res.body.should.have.property('token');
-  //
-  //                     let token = res.body.token;
-  //                     // follow up with requesting user protected page
-  //                     done(); // Don't forget the done callback to indicate we're done!
-  //                     })
-  //                 })
+       chai.request(server)
+           .post('/api/register')
+           .send({
+               "firstname": "Jan",
+               "lastname": "Smit",
+               "email": "jsmit@server.nl",
+               "password": "secret"
+           })
+           .end((err, res) => {
+               res.should.have.status(200);
+               res.body.should.be.a('object');
+               let validToken = res.body.token;
+
+
+               validToken.should.be.a('string')
+
+               done()
+           });
   });
 
   it('should return an error on GET request', (done) => {
       chai.request(server)
         .get('/api/register')
         .end(function (err, res) {
-            res.should.have.status(500);
-            res.body.should.be.a('object');
+            res.should.have.status(404);
 
-            let error = res.body
-            error.should.have.property('message')
-            error.should.have.property('code').equals(500)
-            error.should.have.property('datetime')
             done();
         });
     done();
@@ -56,23 +45,15 @@ describe('Registration', () => {
   it('should throw an error when the user already exists', (done) => {
       chai.request(server)
           .post('api/register')
-          .send([{
+          .send({
               "firstname": "ABC",
               "lastname": "DEF",
-              "email": "abc@def.nl"
-          },{
-              "firstname": "ABC",
-              "lastname": "DEF",
-              "email": "abc@def.nl"
-          }])
+              "email": "abc@def.nl",
+              "password": "string"
+          })
           .end((err, res) => {
-              res.should.have.status(412);
-              res.body.should.be.a('object');
+              res.should.have.status(401);
 
-              let error = res.body
-              error.should.have.property('message')
-              error.should.have.property('code').equals(412)
-              error.should.have.property('datetime')
               done();
           })
     done();
@@ -83,16 +64,12 @@ describe('Registration', () => {
           .post('api/register')
           .send({
               "lastname": "DEF",
-              "email": "abc@def.nl"
+              "email": "abc@def.nl",
+              "password": "string"
           })
           .end((err, res) =>{
               res.should.have.status(412);
-              res.body.should.be.a('object');
 
-              let error = res.body
-              error.should.have.property('message')
-              error.should.have.property('code').equals(412)
-              error.should.have.property('datetime')
               done();
           });
   });
@@ -101,18 +78,14 @@ describe('Registration', () => {
       chai.request(server)
           .post('api/register')
           .send({
-              "firstname": "AB",
+              "firstname": "A",
               "lastname": "DEF",
-              "email": "abc@def.nl"
+              "email": "abc@def.nl",
+              "password": "string"
           })
           .end((err, res) =>{
               res.should.have.status(412);
-              res.body.should.be.a('object');
 
-              let error = res.body
-              error.should.have.property('message')
-              error.should.have.property('code').equals(412)
-              error.should.have.property('datetime')
               done();
           });
   });
@@ -122,16 +95,12 @@ describe('Registration', () => {
           .post('api/register')
           .send({
               "firstname": "DEF",
-              "email": "abc@def.nl"
+              "email": "abc@def.nl",
+              "password": "string"
           })
           .end((err, res) =>{
               res.should.have.status(412);
-              res.body.should.be.a('object');
 
-              let error = res.body
-              error.should.have.property('message')
-              error.should.have.property('code').equals(412)
-              error.should.have.property('datetime')
               done();
           });
   });
@@ -141,17 +110,13 @@ describe('Registration', () => {
           .post('api/register')
           .send({
               "firstname": "ABC",
-              "lastname": "DEF",
-              "email": "abc@def.nl"
+              "lastname": "D",
+              "email": "abc@def.nl",
+              "password": "string"
           })
           .end((err, res) =>{
               res.should.have.status(412);
-              res.body.should.be.a('object');
 
-              let error = res.body
-              error.should.have.property('message')
-              error.should.have.property('code').equals(412)
-              error.should.have.property('datetime')
               done();
           });
   });
@@ -162,94 +127,73 @@ describe('Registration', () => {
           .send({
               "firstname": "ABC",
               "lastname": "DEF",
-              "email": "abcdef.nl"
+              "password": "string"
           })
           .end((err, res) =>{
               res.should.have.status(412);
-              res.body.should.be.a('object');
 
-              let error = res.body
-              error.should.have.property('message')
-              error.should.have.property('code').equals(412)
-              error.should.have.property('datetime')
               done();
           });
   });
 });
 
 describe('Login', () => {
-  it('should return a token when providing valid information', (done) => {
-      chai.request(server)
-          .post('api/login')
-          .send({
-              "firstname": "ABC",
-              "lastname": "DEF",
-              "email": "abc@def.nl"
-          })
-          .end((err, res) =>{
-          res.should.have.status(200);
-          res.body.should.be.a('object');
-          })
 
-    done();
-  });
+    it('should return a token when providing valid information', (done) => {
+        chai.request(server)
+            .post('/api/login')
+            .send({
+                "email": "jsmit@server.nl",
+                "password": "secret"
+            })
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                let validToken = res.body.token;
 
-  it('should throw an error when email does not exist', (done) => {
-      chai.request(server)
-          .post('api/login')
-          .send({
-              "firstname": "ABC",
-              "lastname": "DEF",
-          })
-          .end((err, res) =>{
-              res.should.have.status(412);
-              res.body.should.be.a('object');
 
-              let error = res.body
-              error.should.have.property('message')
-              error.should.have.property('code').equals(412)
-              error.should.have.property('datetime')
-              done();
-          });
-  });
+                validToken.should.be.a('string');
 
-  it('should throw an error when email exists but password is invalid', (done) => {
-      chai.request(server)
-          .post('api/login')
-          .send({
-              "firstname": "ABC",
-              "lastname": "DEF",
-              "email": "abc@def.nl"
-          })
-          .end((err, res) => {
-              res.should.have.status(401);
-              res.body.should.be.a('object');
+                done()
+            });
+    });
+    it('should throw an error when email does not exist', (done) => {
+        chai.request(server)
+            .post('/api/login')
+            .send({
+                "email": "ghdfsa@server.nl",
+                "password": "secret"
+            })
+            .end((err, res) => {
+                res.should.have.status(401)
+                done()
+            });
 
-              let error = res.body
-              error.should.have.property('message')
-              error.should.have.property('code').equals(401)
-              error.should.have.property('datetime')
-              done();
-          })
-  });
+    });
 
-  it('should throw an error when using an invalid email', (done) => {
-    chai.request(server)
-        .post('api/login')
-        .send({
-            "firstname": "ABC",
-            "lastname": "DEF",
-            "email": "abcdef.nl"
-        })
-        .end((err, res) => {
-            res.should.have.status(412);
-            res.body.should.be.a('object');
+    it('should throw an error when email exists but password is invalid', (done) => {
+        chai.request(server)
+            .post('/api/login')
+            .send({
+                "email": "jsmit@server.nl",
+                "password": "thegsadffff"
+            })
+            .end((err, res) => {
+                res.should.have.status(412)
+                done()
+            });
+    });
 
-            let error = res.body
-            error.should.have.property('message')
-            error.should.have.property('code').equals(412)
-            error.should.have.property('datetime')
-            done();
-        })
-  });
+    it('should throw an error when using an invalid email', (done) => {
+        chai.request(server)
+            .post('/api/login')
+            .send({
+                "email": "abcdefg",
+                "password": "secret"
+            })
+            .end((err, res) => {
+                res.should.have.status(412)
+                done()
+            });
+    })
 });
